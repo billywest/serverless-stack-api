75553c017cd7efc23cd2e2b74761194a331400a7
Billy West <billywest.jm@gmail.com>
2020-09-26 00:01:55 -0400

    Adding error logging

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,17 +1,21 @@
+import * as debug from './debug-lib';
+
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
 
+    debug.init(event, context);
+
     try {
-      // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
+      debug.flush(e);
+
       body = { error: e.message };
       statusCode = 500;
     }
 
-    // Return HTTP response
     return {
       statusCode,
       body: JSON.stringify(body),
